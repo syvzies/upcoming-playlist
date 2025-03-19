f42d03c0381f275630ec29f6abc29a80512914a6
syvzies <alex.syverson@gmail.com>
2025-03-19 13:37:44 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,8 +5,6 @@ source "$(dirname "$0")/upcomingplaylist/bin/activate"
 
 # Set Spotify credentials
 # Replace these with your actual values
-export SPOTIFY_CLIENT_ID="d2776941818f424b9153f1ef3844d13c"
-export SPOTIFY_CLIENT_SECRET="d70b2d04c69f4d49ad9318488d98b7d4" 
 export SPOTIFY_PLAYLIST_ID="65Rp5V4nh7QOLma3M3ERsC"
 
 # Run the script with all arguments passed to this script
