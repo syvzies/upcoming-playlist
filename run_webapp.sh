17931626ffa5da50a549113acee07263d1a34c24
syvzies <alex.syverson@gmail.com>
2025-03-19 13:37:25 -0700

    Update run_webapp.sh

--- run_webapp.sh
+++ run_webapp.sh
@@ -29,8 +29,6 @@ pip install -r "$(dirname "$0")/requirements.txt"
 
 # Set Spotify credentials
 # Replace these with your actual values
-export SPOTIFY_CLIENT_ID="d2776941818f424b9153f1ef3844d13c"
-export SPOTIFY_CLIENT_SECRET="d70b2d04c69f4d49ad9318488d98b7d4" 
 export SPOTIFY_PLAYLIST_ID="65Rp5V4nh7QOLma3M3ERsC"
 
 # Redirect URI for web authentication (must match exactly in your Spotify Dashboard settings)
